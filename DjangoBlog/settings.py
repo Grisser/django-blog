1b51c92b8908f1263ead2357b998cc9429c2f672
Вячеслав Рыжков <rugrisser@gmail.com>
2020-01-19 10:30:28 +0300

    startapp

--- DjangoBlog/settings.py
+++ DjangoBlog/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'DjangoBlog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
+        'DIRS': [os.path.join(BASE_DIR, 'proj/templates')]
         ,
         'APP_DIRS': True,
         'OPTIONS': {
