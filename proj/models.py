4153fd0abbc1b3f5c7f44fc84a47dbd6fe39a263
Вячеслав Рыжков <rugrisser@gmail.com>
2020-01-22 22:58:38 +0300

    Favicon + Post Model

--- proj/models.py
+++ proj/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+
+    title = models.CharField(max_length=150)
+    text = models.TextField()
+    publication_time = models.DateTimeField()
+    author = models.IntegerField()
+
+    class Meta:
+        ordering = ["-publication_time"]
+
+    def __str__(self):
+        return self.title
+
